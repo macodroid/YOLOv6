fixed loss function. But still we are missing calculation of canters loss

yolov6/assigners/iou2d_calculator.py
@@ -1,5 +1,5 @@
-#This code is based on
-#https://github.com/fcjian/TOOD/blob/master/mmdet/core/bbox/iou_calculators/iou2d_calculator.py
+# This code is based on
+# https://github.com/fcjian/TOOD/blob/master/mmdet/core/bbox/iou_calculators/iou2d_calculator.py
 
 import torch
 
@@ -53,7 +53,7 @@ def iou2d_calculator(bboxes1, bboxes2, mode='iou', is_aligned=False, scale=1., d
         bboxes2 = cast_tensor_type(bboxes2, scale, dtype)
         overlaps = bbox_overlaps(bboxes1, bboxes2, mode, is_aligned)
         if not overlaps.is_cuda and overlaps.dtype == torch.float16:
-        # resume cpu float32
+            # resume cpu float32
             overlaps = overlaps.float()
         return overlaps
 
@@ -194,14 +194,12 @@ def bbox_overlaps(bboxes1, bboxes2, mode='iou', is_aligned=False, eps=1e-6):
 
     if rows * cols == 0:
         if is_aligned:
-            return bboxes1.new(batch_shape + (rows, ))
+            return bboxes1.new(batch_shape + (rows,))
         else:
             return bboxes1.new(batch_shape + (rows, cols))
 
-    area1 = (bboxes1[..., 2] - bboxes1[..., 0]) * (
-        bboxes1[..., 3] - bboxes1[..., 1])
-    area2 = (bboxes2[..., 2] - bboxes2[..., 0]) * (
-        bboxes2[..., 3] - bboxes2[..., 1])
+    area1 = (bboxes1[..., 2] - bboxes1[..., 0]) * (bboxes1[..., 3] - bboxes1[..., 1])
+    area2 = (bboxes2[..., 2] - bboxes2[..., 0]) * (bboxes2[..., 3] - bboxes2[..., 1])
 
     if is_aligned:
         lt = torch.max(bboxes1[..., :2], bboxes2[..., :2])  # [B, rows, 2]
@@ -218,10 +216,8 @@ def bbox_overlaps(bboxes1, bboxes2, mode='iou', is_aligned=False, eps=1e-6):
             enclosed_lt = torch.min(bboxes1[..., :2], bboxes2[..., :2])
             enclosed_rb = torch.max(bboxes1[..., 2:], bboxes2[..., 2:])
     else:
-        lt = torch.max(bboxes1[..., :, None, :2],
-                       bboxes2[..., None, :, :2])  # [B, rows, cols, 2]
-        rb = torch.min(bboxes1[..., :, None, 2:],
-                       bboxes2[..., None, :, 2:])  # [B, rows, cols, 2]
+        lt = torch.max(bboxes1[..., :, None, :2], bboxes2[..., None, :, :2])  # [B, rows, cols, 2]
+        rb = torch.min(bboxes1[..., :, None, 2:], bboxes2[..., None, :, 2:])  # [B, rows, cols, 2]
 
         wh = fp16_clamp(rb - lt, min=0)
         overlap = wh[..., 0] * wh[..., 1]

yolov6/core/engine.py
@@ -132,11 +132,12 @@ class Trainer:
         with amp.autocast(enabled=self.device != 'cpu'):
             preds, s_featmaps = self.model(images)
             if self.args.distill:
-                with torch.no_grad():
-                    t_preds, t_featmaps = self.teacher_model(images)
-                temperature = self.args.temperature
-                total_loss, loss_items = self.compute_loss_distill(preds, t_preds, s_featmaps, t_featmaps, targets, \
-                                                                   epoch_num, self.max_epoch, temperature)
+                raise Exception("For now, distillation is not supported.")
+                # with torch.no_grad():
+                #     t_preds, t_featmaps = self.teacher_model(images)
+                # temperature = self.args.temperature
+                # total_loss, loss_items = self.compute_loss_distill(preds, t_preds, s_featmaps, t_featmaps, targets, \
+                #                                                    epoch_num, self.max_epoch, temperature)
             else:
                 total_loss, loss_items = self.compute_loss(preds, targets, epoch_num)
             if self.rank != -1:
@@ -218,14 +219,14 @@ class Trainer:
                                         use_dfl=self.cfg.model.head.use_dfl,
                                         reg_max=self.cfg.model.head.reg_max,
                                         iou_type=self.cfg.model.head.iou_type)
-        self.compute_loss_distill = ComputeLoss_distill(num_classes=self.data_dict['nc'],
-                                                        ori_img_size=self.img_size,
-                                                        use_dfl=self.cfg.model.head.use_dfl,
-                                                        reg_max=self.cfg.model.head.reg_max,
-                                                        iou_type=self.cfg.model.head.iou_type,
-                                                        distill_weight=self.cfg.model.head.distill_weight,
-                                                        distill_feat=self.args.distill_feat,
-                                                        )
+        # self.compute_loss_distill = ComputeLoss_distill(num_classes=self.data_dict['nc'],
+        #                                                 ori_img_size=self.img_size,
+        #                                                 use_dfl=self.cfg.model.head.use_dfl,
+        #                                                 reg_max=self.cfg.model.head.reg_max,
+        #                                                 iou_type=self.cfg.model.head.iou_type,
+        #                                                 distill_weight=self.cfg.model.head.distill_weight,
+        #                                                 distill_feat=self.args.distill_feat,
+        #                                                 )
 
     def prepare_for_steps(self):
         if self.epoch > self.start_epoch:

yolov6/data/datasets.py
@@ -165,8 +165,7 @@ class TrainValDataset(Dataset):
 
         if self.augment:
             img, labels = self.general_augment(img, labels)
-
-        labels_out = torch.zeros((len(labels), 6))
+        labels_out = torch.zeros((len(labels), 7))
         if len(labels):
             labels_out[:, 1:] = torch.from_numpy(labels)
 

yolov6/models/effidehead.py
@@ -109,11 +109,12 @@ class Detect(nn.Module):
                 cls_output = torch.sigmoid(cls_output)
                 cls_score_list.append(cls_output.flatten(2).permute((0, 2, 1)))
                 reg_distri_list.append(reg_output.flatten(2).permute((0, 2, 1)))
+                # TODO: add sigmoid to centers
                 centers_list.append(centers_output.flatten(2).permute((0, 2, 1)))
 
-            cls_score_list = torch.cat(cls_score_list, axis=1)
-            reg_distri_list = torch.cat(reg_distri_list, axis=1)
-            centers_list = torch.cat(centers_list, axis=1)
+            cls_score_list = torch.cat(cls_score_list, dim=1)
+            reg_distri_list = torch.cat(reg_distri_list, dim=1)
+            centers_list = torch.cat(centers_list, dim=1)
 
             return x, cls_score_list, reg_distri_list, centers_list
         else:
@@ -300,7 +301,7 @@ def build_effidehead_layer(channels_list, num_anchors, num_classes, reg_max=16):
         # centers_pred2
         nn.Conv2d(
             in_channels=channels_list[10],
-            out_channels=4 * (reg_max + num_anchors),
+            out_channels=1,
             kernel_size=1
         )
     )

yolov6/models/loss.py
@@ -56,7 +56,7 @@ class ComputeLoss:
             epoch_num
     ):
 
-        feats, pred_scores, pred_distri = outputs
+        feats, pred_scores, pred_distri, centers_pred = outputs
         anchors, anchor_points, n_anchors_list, stride_tensor = \
             generate_anchors(feats, self.fpn_strides, self.grid_cell_size, self.grid_cell_offset,
                              device=feats[0].device)
@@ -68,7 +68,10 @@ class ComputeLoss:
         # targets
         targets = self.preprocess(targets, batch_size, gt_bboxes_scale)
         gt_labels = targets[:, :, :1]
-        gt_bboxes = targets[:, :, 1:]  # xyxy
+        gt_bboxes = targets[:, :, 1:5]  # xyxy
+        # TODO: come up with way how to calculate loss for centers.
+        # For now, predicted centers are in shape [batch_size, num_anchors, output (1)]
+        gt_centers = targets[:, :, -1]
         mask_gt = (gt_bboxes.sum(-1, keepdim=True) > 0).float()
 
         # pboxes
@@ -119,7 +122,7 @@ class ComputeLoss:
                        (self.loss_weight['class'] * loss_cls).unsqueeze(0))).detach()
 
     def preprocess(self, targets, batch_size, scale_tensor):
-        targets_list = np.zeros((batch_size, 1, 5)).tolist()
+        targets_list = np.zeros((batch_size, 1, 6)).tolist()
         for i, item in enumerate(targets.cpu().numpy().tolist()):
             targets_list[int(item[0])].append(item[1:])
         max_len = max((len(l) for l in targets_list))
@@ -127,7 +130,7 @@ class ComputeLoss:
             np.array(list(map(lambda l: l + [[-1, 0, 0, 0, 0]] * (max_len - len(l)), targets_list)))[:, 1:, :]).to(
             targets.device)
         batch_target = targets[:, :, 1:5].mul_(scale_tensor)
-        targets[..., 1:] = xywh2xyxy(batch_target)
+        targets[..., 1:5] = xywh2xyxy(batch_target)
         return targets
 
     def bbox_decode(self, anchor_points, pred_dist):