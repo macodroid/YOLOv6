696c223fd815db55afe0b9f7b39816a2878b910b
maco <macko.andrej1@gmail.com>
2023-04-06 11:17:31 +0200

    formating

--- infer.py
+++ infer.py
@@ -2,8 +2,8 @@
 # -*- coding:utf-8 -*-
 import argparse
 import os
-import sys
 import os.path as osp
+import sys
 
 import torch
 
@@ -20,9 +20,11 @@ def get_args_parser(add_help=True):
     parser.add_argument('--weights', type=str, default='weights/yolov6s.pt', help='model path(s) for inference.')
     parser.add_argument('--source', type=str, default='data/images', help='the source path, e.g. image-file/dir.')
     parser.add_argument('--webcam', action='store_true', help='whether to use webcam.')
-    parser.add_argument('--webcam-addr', type=str, default='0', help='the web camera address, local camera or rtsp address.')
+    parser.add_argument('--webcam-addr', type=str, default='0',
+                        help='the web camera address, local camera or rtsp address.')
     parser.add_argument('--yaml', type=str, default='data/coco.yaml', help='data yaml file.')
-    parser.add_argument('--img-size', nargs='+', type=int, default=[640, 640], help='the image-size(h,w) in inference size.')
+    parser.add_argument('--img-size', nargs='+', type=int, default=[640, 640],
+                        help='the image-size(h,w) in inference size.')
     parser.add_argument('--conf-thres', type=float, default=0.4, help='confidence threshold for inference.')
     parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold for inference.')
     parser.add_argument('--max-det', type=int, default=1000, help='maximal inferences per image.')
@@ -31,7 +33,8 @@ def get_args_parser(add_help=True):
     parser.add_argument('--not-save-img', action='store_true', help='do not save visuallized inference results.')
     parser.add_argument('--save-dir', type=str, help='directory to save predictions in. See --save-txt.')
     parser.add_argument('--view-img', action='store_true', help='show inference results')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by classes, e.g. --classes 0, or --classes 0 2 3.')
+    parser.add_argument('--classes', nargs='+', type=int,
+                        help='filter by classes, e.g. --classes 0, or --classes 0 2 3.')
     parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS.')
     parser.add_argument('--project', default='runs/inference', help='save inference results to project/name.')
     parser.add_argument('--name', default='exp', help='save inference results to project/name.')
@@ -105,7 +108,8 @@ def run(weights=osp.join(ROOT, 'yolov6s.pt'),
 
     # Inference
     inferer = Inferer(source, webcam, webcam_addr, weights, device, yaml, img_size, half)
-    inferer.infer(conf_thres, iou_thres, classes, agnostic_nms, max_det, save_dir, save_txt, not not_save_img, hide_labels, hide_conf, view_img)
+    inferer.infer(conf_thres, iou_thres, classes, agnostic_nms, max_det, save_dir, save_txt, not not_save_img,
+                  hide_labels, hide_conf, view_img)
 
     if save_txt or not not_save_img:
         LOGGER.info(f"Results saved to {save_dir}")
